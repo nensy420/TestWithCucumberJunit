239f1aaf9d68c192788a1030abae2dba492c9a39
Olha_Varvarska <nensy>
2018-09-20 15:13:29 +0300

    add some ChangeForMergeMaster

--- src/test/java/stepsdef/HeadersLinksStepsDef.java
+++ src/test/java/stepsdef/HeadersLinksStepsDef.java
@@ -3,4 +3,8 @@ package stepsdef;
 
 public class HeadersLinksStepsDef {
 
+    public static String ghjkkllhihf;
+    public static String ghjkkllhihf1;
+    public static String ghjkkllhihf2;
+    public static String ghjkkllhihf3;
 }
