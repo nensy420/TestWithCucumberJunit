e66c570047ce41505a26ed4b6ba92878b1dec823
Olha_Varvarska <nensy>
2018-09-20 15:09:01 +0300

    commit1

--- src/test/java/stepsdef/HeadersLinksStepsDef.java
+++ src/test/java/stepsdef/HeadersLinksStepsDef.java
@@ -1,6 +1,14 @@
 package stepsdef;
 
+import org.openqa.selenium.WebElement;
+
 
 public class HeadersLinksStepsDef {
+
+    private WebElement fghjkl;
+    private WebElement fghjkl1;
+    private WebElement fghjkl2;
+    private WebElement fghjkl3;
+
 
 }
